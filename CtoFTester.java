2268cf23dc0ea7e6ab6d4abbe4a15d855dd7b9e5
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-09-18 21:17:47 -0400

    First change

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,11 +1,8 @@
 public class CtoFTester {
-
-	public static void main(String[] args) {
-		System.out.println("CtoFTester");
-}
-
-
-
-
-
+	public double celsiusToFahrenheit(double Cel) {
+	return (Cel * 9 / 5) + 32;
+	}
+        public double fahrenheitToCelsius(double Far) {
+	return (Far - 32) * 5 / 9;
+        }
 }
